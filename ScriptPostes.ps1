7c0b8ee9b9ed53c9153027830c6f0a14bda14509
samgon <samuel.gonzales@ac-strasbourg.fr>
2017-10-16 10:28:53 +0200

    Update ScriptPostes.ps1

--- ScriptPostes.ps1
+++ ScriptPostes.ps1
@@ -31,6 +31,7 @@ write-host "Liste des membres du groupe Administrateurs :"
 #
 #Samba domain
 #
+write-host "Configuration accès au domaine Samba"
 $RegKey="HKLM:\System\CurrentControlSet\Services\LanManWorkstation\Parameters"
 $RegKeyName="DNSNameResolutionRequired"
 $RegKeyValue="0"
@@ -38,9 +39,11 @@ set-ItemProperty $RegKey -Name $RegKeyName -Value $RegKeyValue
 $RegKeyName="DomainCompatibilityMode"
 $RegKeyValue="1"
 Set-ItemProperty $RegKey -Name $RegKeyName -Value $RegKeyValue
+write-host "Fait!"
 #
 #Verrouillage numlock
 #
+write-host "Verrouillage Numlock"
 $RegKey="HKCU:\Control Panel\Keyboard"
 $RegKeyName="InitialKeyboardIndicators"
 $RegKeyValue="2"
@@ -50,9 +53,11 @@ $RegKey="HKU:\.DEFAULT\Control Panel\Keyboard"
 $RegKeyName="InitialKeyboardIndicators"
 $RegKeyValue="2"
 Set-ItemProperty $RegKey -Name $RegKeyName -Value $RegKeyValue
+write-host "Fait!"
 #
 #win10 accès netlogon
 #
+write-host "Accès au netlogon Win10"
 $RegKey="HKLM:\SOFTWARE\Policies\Microsoft\Windows\NetworkProvider\HardenedPaths"
 $RegKeyName="\\\\*\\NETLOGON"
 $RegKeyValue="RequireMutualAuthentication=0,RequireIntegrity=0"
@@ -67,15 +72,22 @@ Set-ItemProperty $RegKey -Name $RegKeyName -Value $RegKeyValue
 $RegKeyName="\\\\*\\SYSVOL"
 $RegKeyValue="RequireMutualAuthentication=0,RequireIntegrity=0,RequirePrivacy=0"
 Set-ItemProperty $RegKey -Name $RegKeyName -Value $RegKeyValue
+write-host "Fait!"
 #
 #Creation dossier bureau assistance
 #
+write-host "Création du dossier assistance"
 New-Item -Path "C:\Users\Public\Desktop\@ASSISTANCE"  -ItemType Directory
 #supression
 #remove-Item -Path "C:\Users\Public\Desktop\@ASSISTANCE"
+write-host "Copie des elements sur le bureau public"
+#
+Copy-Item -Recurse -Path "c:\SG-w10-install\@ASSISTANCE\*" -Destination "C:\Users\Public\Desktop\@ASSISTANCE" -Verbose
+write-host "Fait!"
 #
 #Creation du reseau.bat
 #
+write-host "Accès aux lecteurs réseau"
 Add-Content -Path "C:\Windows\reseau.bat" -Value "@echo off
 cls
 ECHO ************************************************************************
@@ -86,11 +98,7 @@ ECHO ************************************************************************
 ECHO *                Test de presence du serveur HORUS                     *
 ECHO ************************************************************************
 ping $IP_HORUS -n 1
-
-
 \\$IP_HORUS\netlogon\scripts\groups\DomainUsers.bat
-
-
 :fin
 ECHO ************************************************************************
 ECHO *                        Erreur de NETLOGON                            *
@@ -106,9 +114,24 @@ $RegKeyValue="c:\\windows\\reseau.bat"
 Set-ItemProperty $RegKey -Name $RegKeyName -Value $RegKeyValue
 write-host "Réseau.bat ajouté à l'ouverture de session"
 #
-#copie des elements sur le bureau public
+#supprimer animation lors du 1er logon
 #
-Copy-Item -Recurse -Path .\seven\bureau\users\Public\Desktop\* -Destination "C:\Users\Public\Desktop" -Verbose
-Get-PSDrive
-pwd
-Read-Host
+write-host "Je supprime l'animation lors du premier logon"
+$RegKey="HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$RegKeyName="EnableFirstLogonAnimation"
+$RegKeyValue="0"
+set-ItemProperty $RegKey -Name $RegKeyName -Value $RegKeyValue
+write-host "Fait!"
+#
+#Cortana remplacé par winsearch
+#
+write-host "Je remplace Cortana par WindowsSearch"
+$RootPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\"
+$CortanaPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+if (!(Test-Path -Path $CortanaPath)) {
+New-Item -Path $RootPath -Name "Windows Search"
+}
+Set-ItemProperty $CortanaPath -Name "AllowCortana" -Value 0 #mettre la valeur à 1 pour réactiver cortana
+Stop-Process -Name explorer
+write-host "Fait!"
+Read-Host "Fin du script, appuyer sur ENTREE pour continuer"
